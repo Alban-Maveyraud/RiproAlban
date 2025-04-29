7f7f60374b9b628ad7b9d05294df7e11feb518cd
Taieb Yannis <yannis.taieb@etu.univ-cotedazur.fr>
2025-04-29 13:06:50 +0200

    j

--- src/app/maquetteJeu/maquetteJeu.component.ts
+++ src/app/maquetteJeu/maquetteJeu.component.ts
@@ -55,13 +55,10 @@ export class MaquetteJeuComponent implements OnInit {
       this.router.navigate(['/jeu-voiture']);
     }, 3000);
   }
-
-  goToAdmin() {
-    const password = prompt('Entrez le mot de passe admin :');
-    if (password === 'caca') {
-      this.router.navigate(['/config']);
-    } else {
-      alert('Mot de passe incorrect !');
-    }
+  
+  quitter(): void {
+    this.router.navigate(['']);
   }
+  
+
 }
